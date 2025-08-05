5eaf62950078185fa68dd35319fab34f01c1fdab
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-05 10:44:13 +0530

    Add tests for initial supply and owner minting in OceanTokenTest contract

--- ocean-faucet-contract/test/OceanToken.t.sol
+++ ocean-faucet-contract/test/OceanToken.t.sol
@@ -28,9 +28,42 @@ contract OceanTokenTest is Test {
 
     // ---------------------------- Tests ----------------------------
 
+    /**
+     * @notice Test the initial supply of the OceanToken contract.
+     * @dev This test checks if the initial supply is set correctly to 1 million tokens
+     */
     function test_InitialSupply() public view {
         uint256 initialSupply = token.totalSupply();
         console2.log("Initial Supply:", initialSupply);
         assertEq(initialSupply, INITIAL_SUPPLY, "Initial supply should be 1 million tokens");
+    }
+
+    /**
+     * @notice Test the check new mint amount.
+     * @dev This test checks if the owner can mint new tokens and the total supply updates accordingly.
+     */
+    function test_OwnerMint() public {
+        vm.startPrank(owner);
+
+        uint256 mintAmount = 100 * 10 ** 18; // 100 OCT
+
+        // Mint tokens to the owner
+        token.mint(owner, mintAmount);
+
+        // Check the owner's balance after minting
+        uint256 ownerBalance = token.balanceOf(owner);
+        console2.log("Owner Balance after minting:", ownerBalance);
+        assertEq(ownerBalance, INITIAL_SUPPLY + mintAmount, "Owner balance should be initial supply plus minted amount");
+
+        // Check the total supply after minting
+        uint256 totalSupplyAfterMint = token.totalSupply();
+        console2.log("Total Supply after minting:", totalSupplyAfterMint);
+        assertEq(
+            totalSupplyAfterMint,
+            INITIAL_SUPPLY + mintAmount,
+            "Total supply should be initial supply plus minted amount"
+        );
+
+        vm.stopPrank();
     }
 }
